0e6bd8604de1bab33b1315262a30d8675eaba970
Roberto Salama <102178835+ROBER-SALAMA@users.noreply.github.com>
2023-07-26 22:12:21 -0600

    modifiue el JS

--- assets/js/validaciones.js
+++ assets/js/validaciones.js
@@ -1,30 +1,36 @@
-export  function valida (input) {
-    const tipoDeInput = input.dataset.tipo
-    if (validadores[tipoDeInput]) {
-        validadores[tipoDeInput](input)
-    }
+export function valida(input) {
+  const tipoDeInput = input.dataset.tipo;
+  if (validadores[tipoDeInput]) {
+    validadores[tipoDeInput](input);
+  }
+
+  if (input.validity.valid) {
+    input.parenElement.classList.remove("input-container--invalid");
+  }else{
+    input.parenElement.classList.add("input-container--invalid");
+  }
 }
 
 const validadores = {
-    nacimiento: input => validarNacimiento(input)
-}
+  nacimiento: (input) => validarNacimiento(input),
+};
 
 function validarNacimiento(input) {
-    const fechaCliente = new Date(input.value);
-    //establese el mensaje de error
-    let mensaje = "";
-    if (!mayorDeEdad(fechaCliente)) {
-        mensaje = 'Debes ser mayor de edad.';
-    }
-    input.setCustomValidity(mensaje); // Establecer mensaje de error personalizado
+  const fechaCliente = new Date(input.value);
+  let mensaje = "";
+  if (!mayorDeEdad(fechaCliente)) {
+    mensaje = "Debes tener al menos 18 años de edad";
+  }
+
+  input.setCustomValidity(mensaje);
 }
 
 function mayorDeEdad(fecha) {
-    const fechaActual = new Date();
-    const diferenciaFechas = new Date(
-        fecha.getUTCFullYear() + 18,
-        fecha.getUTCMonth(),
-        fecha.getUTCDate()
-    );
-    return diferenciaFechas <= fechaActual;
+  const fechaActual = new Date();
+  const diferenciaFechas = new Date(
+    fecha.getUTCFullYear() + 18,
+    fecha.getUTCMonth(),
+    fecha.getUTCDate()
+  );
+  return diferenciaFechas <= fechaActual;
 }
